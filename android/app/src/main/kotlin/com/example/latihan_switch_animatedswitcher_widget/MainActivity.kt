4c148d6e9acfa05920b27e7ea1edff07d8eac312
luizmirrelll <aloysius.mirel96@gmail.com>
2020-10-30 03:03:25 +0700

    Switch & AnimatedSwitcher Widget

---
+++ android/app/src/main/kotlin/com/example/latihan_switch_animatedswitcher_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.latihan_switch_animatedswitcher_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isOn = false;
+  Widget myWidget = Container(
+    width: 200,
+    height: 100,
+    decoration: BoxDecoration(
+      color: Colors.red,
+      border: Border.all(color: Colors.black, width: 3),
+    ),
+  );
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Switch & AnimatedSwitcher Widget"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              AnimatedSwitcher(
+                child: myWidget,
+                duration: Duration(seconds: 1),
+                transitionBuilder: (child, animation) => RotationTransition(
+                  turns: animation,
+                  child: child,
+                ),
+              ),
+              Switch(
+                  activeColor: Colors.green,
+                  inactiveThumbColor: Colors.red,
+                  inactiveTrackColor: Colors.red[200],
+                  value: isOn,
+                  onChanged: (newValue) {
+                    isOn:
+                    newValue;
+                    setState(() {
+                      if (isOn)
+                        myWidget = Container(
+                          key: ValueKey(1),
+                          width: 200,
+                          height: 100,
+                          decoration: BoxDecoration(
+                              color: Colors.green,
+                              border:
+                                  Border.all(color: Colors.black, width: 3)),
+                        );
+                      // myWidget = SizedBox(
+                      //   width: 200,
+                      //   height: 100,
+                      //   child: Center(
+                      //     child: Text(
+                      //       "Swicth ON",
+                      //       style: TextStyle(
+                      //           color: Colors.green,
+                      //           fontWeight: FontWeight.w700,
+                      //           fontSize: 20),
+                      //     ),
+                      //   ),
+                      // );
+                      else
+                        myWidget = Container(
+                          key: ValueKey(2),
+                          width: 200,
+                          height: 100,
+                          decoration: BoxDecoration(
+                              color: Colors.red,
+                              border:
+                                  Border.all(color: Colors.black, width: 3)),
+                        );
+                    });
+                  })
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
